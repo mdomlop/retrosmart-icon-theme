00c5b48b1a1f9844fb29d76abd55a3f4fd09fa72
root <root@asrock.home.lan>
2016-08-24 11:22:04 +0200

    Updated from makefile

--- src/mkpreview.sh
+++ src/mkpreview.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-montage -resize 64 -geometry +16+16 -tile 9x6 \
+montage -verbose -resize 64 -geometry +16+16 -tile 9x6 \
     src/images/{$(tr '\n' , < src/preview.list | rev | cut -c2- | rev)} \
     preview.png
